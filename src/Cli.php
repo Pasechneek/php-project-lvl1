206b9f204a491a263d77aa720a7caa8007ed923b
Danil Merkurev <60576638+Pasechneek@users.noreply.github.com>
2021-07-15 10:22:45 +0300

    Update Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -6,6 +6,6 @@ namespace Brain\Games\Cli;
   use function cli\prompt;
 
   line('Welcome to the Brain Game!');
-  //$name = prompt('May I have your name?');
+  $name = prompt('May I have your name?');
   $name = trim(fgets(STDIN));
   line("Hello, %s!", $name);
