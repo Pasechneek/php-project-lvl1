06746985df6ab10e5a44233c69d9482360c04fed
Pasechneek <danil.merkurev@yandex.com>
2021-07-19 15:57:36 +0300

    step 5.2

--- src/Numbers.php
+++ src/Numbers.php
@@ -15,10 +15,9 @@ function numbers(): string
     line("Hello, %s!", $name);
 
     line('Answer "yes" if the number is even, otherwise answer "no".');
-    $fails = 0;
 
     for ($i = 0; $i < 3; $i++) {
-        $randomNumber = rand(0, 99);
+        $randomNumber = rand(1, 99);
         line("Question: {$randomNumber}");
         $answer = prompt('Your answer: ');
         $isEven = $randomNumber % 2 === 0;
@@ -27,14 +26,10 @@ function numbers(): string
             line('Correct!');
         } else {
             line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAncver}'.");
-            $fails++;
+            line("Let's try again, {$name}.");
+            return $name;
         }
     }
-    if ($fails === 0) {
-        line("Congratulations, {$name}!");
+    line("Congratulations, {$name}!");
         return $name;
-    } else {
-        line("Let's try again, {$name}.");
-        return $name;
-    }
 }
