54402cb50e07b4f6d7ceeb35cd3fd9f859d78708
Pasechneek <danil.merkurev@yandex.com>
2021-07-27 18:34:34 +0300

    9.5

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -5,13 +5,10 @@ namespace Brain\Games\Even;
 use Psy\Util\Str;
 
 use function cli\line;
-use function Brain\Games\Cli\cli;
 use function Brain\Games\Src\Engine\engine;
 
-function even(): void
+function even($name): void
 {
-    line('Answer "yes" if the number is even, otherwise answer "no".');
-    $name = cli();
     for ($i = 0; $i < 3; $i++) {
         $randomNumber = rand(1, 99);
         $isEven = $randomNumber % 2 === 0;
