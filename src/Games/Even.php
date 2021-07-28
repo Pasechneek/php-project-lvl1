d24b80664c0cd90d1ff7182bfd3ceaaaa3ddc3eb
Pasechneek <danil.merkurev@yandex.com>
2021-07-28 17:33:57 +0300

    9.27

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -10,12 +10,13 @@ use function Brain\Games\Src\Engine\engine;
 function even(string $name): void
 {
     $attempts = 3;
+    $result = '';
     for ($i = 0; $i < $attempts; $i++) {
         $result = '';
         $randomNumber = rand(1, 99);
         $isEven = $randomNumber % 2 === 0;
         $correctAncver = $isEven ? 'yes' : 'no';
-        $question = $randomNumber;
+        $question = (string) $randomNumber;
 
         $engine = engine($question, $correctAncver);
 
